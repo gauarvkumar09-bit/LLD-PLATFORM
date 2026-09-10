ffb49f95e52a740df31fdbe04380e71207d26915
gaurav <gauarvkumar09-bit>
2026-09-11 02:21:27 +0530

    deploying

--- frontend/src/pages/HistoryPage.jsx
+++ frontend/src/pages/HistoryPage.jsx
@@ -9,7 +9,7 @@ export const HistoryPage = () => {
 
   useEffect(() => {
     axios
-      .get('http://localhost:5000/api/submissions/history', { withCredentials: true })
+      .get(`${import.meta.env.VITE_API_URL}/submissions/history`, { withCredentials: true })
       .then((res) => setHistory(res.data))
       .catch((err) => console.error(err))
       .finally(() => setLoading(false));
